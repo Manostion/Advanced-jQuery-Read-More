09ce4cce714b3946cae0f7d718a54e8f57bd4fb4
David Whitworth <david@whitworth.de>
2016-09-08 11:55:57 +0200

    Version 1.1 - Added "linking" option - Added "data-linking" and "data-duration" attributes - Fixes

--- js/advanced-read-more.js
+++ js/advanced-read-more.js
@@ -16,6 +16,7 @@ $.fn.readMore = function(options) {
     var settings = $.extend({
         showLines: 8,                               // sets the number of lines to display; i.e. the ninth line and all thereafter will be hidden
         showParagraph: false,                       // if set to true, the plugin will cut off after the first paragraph instead of a number of lines ---> overwrites showLines
+        linking: true,                              // if set to false, the text will just be truncated without a "read more" link
         linkCaption: 'read more',                   // defines the caption of the read more toggle
         linkCloseCaption: 'read less',              // defines the caption of the read less toggle
         linkHint: 'Click to see full article',      // sets the title (i.e. the mouseover hint) of the read more toggle
@@ -38,7 +39,7 @@ $.fn.readMore = function(options) {
 //            lineHeight = $(this).children(':first-child').css('line-height');
             $(this).children(':first-child').append('<div id="Temp">&nbsp;</div>');
             lineHeight = $('#Temp').height();
-            $('#Temp').remove;
+            $('#Temp').remove();
             // <-- get line-height //
             
             // transfer first child's margin-top to the container to keep elements within --> //
@@ -78,7 +79,13 @@ $.fn.readMore = function(options) {
             // reduce content if necessary and append "read more" link --> //
             if (sectionFullHeight > sectionHeight) {
                 $(this).css('height', sectionHeight).addClass('jrm-reduced');
-                $('<span class="jrm-toggle" data-id="' + sectionId + '" title="' + settings.linkHint + '">' + linkCaption + '</span>').insertAfter($(this));
+                if (settings.linking || $(this).attr('data-linking') == 'true' || $(this).attr('data-linking') == 'open only') {
+                    $('<span class="jrm-toggle" data-id="' + sectionId + '" title="' + settings.linkHint + '">' + linkCaption + '</span>').insertAfter($(this));
+                } else if (!setting.linking || $(this).attr('data-linking') != 'true') {
+                    $(this).prepend('<p id="Temp">&nbsp;</p>');
+                    $(this).css('margin-bottom', $('#Temp').css('margin-bottom'));
+                    $('#Temp').remove();
+                }
                 if (settings.previewTextOnly) {
                     $(this).addClass('jrm-textOnly');
                 }
@@ -89,8 +96,19 @@ $.fn.readMore = function(options) {
         // "read more" / "read less" link functionality --> //
         $('.jrm-toggle').click(function() {
             var toggleLink = $(this),
-                childrenContainer = toggleLink.parent().children('.jrm-' + toggleLink.attr('data-id'));
+                childrenContainer = toggleLink.parent().children('.jrm-' + toggleLink.attr('data-id')),
+                animationDuration;
 
+            if (settings.linking != true || childrenContainer.attr('data-linking') == 'open only') {
+                toggleLink.remove();
+            }
+            
+            if (childrenContainer.attr('data-duration')) {
+                animationDuration = parseFloat(childrenContainer.attr('data-duration'));
+            } else {
+                animationDuration = settings.animationSpeed;
+            }
+            
             // define the reduced-state height of the container either as lines * line-height or as first paragraph's height --> //
             if (!settings.showParagraph && childrenContainer.attr('data-lines') != -1) {
                 if (childrenContainer.attr('data-lines')) {
@@ -129,25 +147,25 @@ $.fn.readMore = function(options) {
                 for (i = 1; i < childrenCount + 1; i ++) {
                     childrenHeight = childrenHeight + childrenContainer.children(':nth-child(' + i + ')').outerHeight(true)
                 }
-                
+
                 childrenContainer.animate( {
                     height: childrenHeight
-                }, settings.animationSpeed);
+                }, animationDuration);
                 
                 setTimeout(function() {
                     toggleLink.text(linkCloseCaption).attr('title', '');
                     $(window).resize();
-                }, settings.animationSpeed);
+                }, animationDuration);
             } else {
                 childrenContainer.animate( {
                     height: sectionHeight
-                }, settings.animationSpeed);
+                }, animationDuration);
                 
                 setTimeout(function() {
                     toggleLink.text(linkCaption).attr('title', settings.linkHint);
                     childrenContainer.addClass('jrm-reduced');
                     $(window).resize();
-                }, settings.animationSpeed);
+                }, animationDuration);
             }
         });
         // <-- "read more" / "read less" link functionality //
